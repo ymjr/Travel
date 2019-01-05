4182926c83d94cadbef5ccb038992267c4a4eede
tangym123 <1974895947@qq.com>
2019-01-05 19:45:58 +0800

    "change"

---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption" >
+      <!-- slides -->
+      <swiper-slide v-for="item in swiperList" :key="item.id">
+        <img class="swiper-img" :src="item.imgUrl"/>
+      </swiper-slide>
+      <!-- Optional controls -->
+      <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        autoplay: 3000,
+        loop: true,
+        pagination: '.swiper-pagination'
+      },
+      swiperList: [{
+        id:'0001',
+        imgUrl:'http://img1.qunarzz.com/piao/fusion/1812/d6/daa880b254940402.jpg_750x200_b114308a.jpg'
+      },
+     {
+        id:'0002',
+        imgUrl:'http://img1.qunarzz.com/piao/fusion/1811/31/da037478f37cf202.jpg_750x200_fe28d396.jpg'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .wrapper >>> .swiper-pagination-bullet-active
+    background :#ffffff
+  .wrapper
+    overflow:hidden
+    width: 100%
+    height:0
+    padding-bottom:26.67%
+    background :#eee
+    .swiper-img
+      width :100%;
+</style>
