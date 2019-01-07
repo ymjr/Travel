4a0b824934e9a0aca2a25b989b02646770c081f7
tangym123 <1974895947@qq.com>
2019-01-07 15:53:08 +0800

    "CityRouter"

---
+++ src/pages/city/City.vue
@@ -0,0 +1,18 @@
+<template>
+<city-header></city-header>
+</template>
+
+<script>
+import CityHeader from './components/header'
+
+export default {
+  name: 'City',
+  components: {
+    CityHeader
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -7,10 +7,12 @@
       <span class="iconfont">&#xe632;</span>
       输入城市/景点/游玩主题
     </div>
-    <div class="header-right">
-      {{city}}
-      <span class="iconfont arrow-icon">&#xe64a;</span>
-    </div>
+    <router-link to="/city">
+      <div class="header-right">
+        {{city}}
+        <span class="iconfont arrow-icon">&#xe64a;</span>
+      </div>
+    </router-link>
   </div>
 </template>
 
@@ -27,9 +29,10 @@ export default {
   @import "~&styles/varibles.styl"
   .header
     display: flex
-    line-height: .86rem
+    line-height: $headerHeight
     background-color: $bgColor
     color: #fff
+    height :$headerHeight
     .header-left
       width: .64rem
       float: left
@@ -50,6 +53,7 @@ export default {
       width: 1.24rem
       float: right
       text-align: center
+      color:#ffffff
       .arrow-icon
         font-size: .24rem
         margin-left: -.04rem
